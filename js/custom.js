3039a4557fd82ed41836f887126ff7339d7ae2ef
ShubhamSatnurkar <shubhamsatnurkar061@gmail.com>
2022-03-23 19:51:43 +0530

    responsive work completed for half webpage

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $("#navbar-toogle").on('click', function(){
-        $("#nav").toggleClass("hidden")
+        $("#nav").toggleClass("hidden").css({
+            'background': 'linear-gradient(rgba(0,0,0,.4),rgba(5,47,28,.7), rgba(5,47,28,1)  )',
+            'transition': '1s',
+            'right': '0px',
+            'width': '66%'
+        })
+        
     })
 })
